1076d89354a851837546533cfb26505577d7a1ae
amr roshdy shanab <120336819+am172@users.noreply.github.com>
2025-09-16 18:39:16 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://accounting-management-abjr.onrender.com',
   credentials: true
 }));
 app.use(express.json());
