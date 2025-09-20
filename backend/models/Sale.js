51b6ec910a20a115872abd18550900b649f45f03
amr roshdy shanab <120336819+am172@users.noreply.github.com>
2025-09-20 04:53:17 +0300

    Update Sale.js

--- backend/models/Sale.js
+++ backend/models/Sale.js
@@ -2,15 +2,16 @@
 const mongoose = require('mongoose');
 
 const saleSchema = new mongoose.Schema({
-  serialNumber: String,   // ✅ رقم السريال
+  invoiceNumber: { type: Number, unique: true }, // ✅ رقم الفاتورة
+  serialNumber: String,
   productName: String,
-  type: String,           // ✅ عربية / قطعة
-  supplier: String,       // ✅ المورد
-  model: String,          // ✅ الموديل
-  manufactureYear: Number,// ✅ سنة الصنع
-  color: String,          // ✅ اللون
-  chassisNumber: String,  // ✅ رقم الشاسيه
-  condition: String,      // ✅ الحالة (جديد / مستعمل)
+  type: String,
+  supplier: String,
+  model: String,
+  manufactureYear: Number,
+  color: String,
+  chassisNumber: String,
+  condition: String,
   price: Number,
   buyer: String,
   quantity: Number,
@@ -20,4 +21,3 @@ const saleSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Sale', saleSchema);
-
